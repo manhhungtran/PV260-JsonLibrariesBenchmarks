fc099151ca947f997d1be3e187c09ff6ff7f533d
Manh Hung Tran <hungmanh.t94@gmail.com>
2018-05-15 16:40:31 +0200

    Create Json serialize benchmark

---
+++ JsonBenchmark/JsonSerializersBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using JsonBenchmark.TestDTOs;
+using Newtonsoft.Json;
+
+namespace JsonBenchmark
+{
+    [ClrJob(isBaseline: true)]
+    [RPlotExporter, RankColumn]
+    [HtmlExporter]
+    public class JsonSerializersBenchmarks : JsonBenchmarkBase
+    {
+        private Root _root;
+
+        [GlobalSetup]
+        public void SetUp()
+        {
+            _root = JsonConvert.DeserializeObject<Root>(JsonSampleString);
+        }
+
+        [Benchmark]
+        public string NewtonsoftJson_Serialize()
+        {
+            return JsonConvert.SerializeObject(_root);
+        }
+
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _root = null;
+        }
+    }
+}
